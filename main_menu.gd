2c054e71c58dc74743ef45c3eec3c655626adc3c
battle906 <rbeeh@outlook.com.au>
2026-05-05 10:17:07 +1000

    major improvements
    this update adds lots of functional stuff

--- main_menu.gd
+++ main_menu.gd
@@ -1,13 +1,27 @@
+class_name MainMenu
 extends Node2D
+@onready var start_button = $buttonmanager/start as Button
+@onready var exit_button = $buttonmanager/quit as Button
+@onready var options_button = $buttonmanager/options as Button
+@onready var main_game = preload("res://main_game.tscn") as PackedScene
+@onready var options_menu = preload("res://options.tscn") as PackedScene
+
+func free() -> void:
+	handle_connecting_signals()
+
+func on_start_pressed():
+	get_tree().change_scene_to_packed(main_game)
 
 
-func _on_start_pressed():
-	get_tree().change_scene_to_file("res://main_game.tscn")
-
-
-func _on_options_pressed():
+func on_options_pressed():
 	get_tree().change_scene_to_file("res://options.tscn")
 
 
-func _on_quit_pressed():
+func on_quit_pressed():
 	get_tree().quit()
+
+
+func handle_connecting_signals() -> void:
+	start_button.button_down.connect(on_start_pressed)
+	options_button.button_down.connect(on_options_pressed)
+	exit_button.button_down.connect(on_quit_pressed)
